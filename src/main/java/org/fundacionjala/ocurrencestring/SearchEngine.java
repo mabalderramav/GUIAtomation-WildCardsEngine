f7bb6ffbfd9d53e997074fcedc43267c972d839d
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2016-09-06 18:06:31 -0400

    improve code

--- src/main/java/org/fundacionjala/ocurrencestring/SearchEngine.java
+++ src/main/java/org/fundacionjala/ocurrencestring/SearchEngine.java
@@ -7,9 +7,23 @@ import java.util.regex.Pattern;
  * Created by AldoBalderrama on 9/2/2016.
  */
 public class SearchEngine {
-    final static String UNDERSCORE = "_";
-    public static int find(String word, String text){
-        String regWord = "\\Q" + word.replaceAll(UNDERSCORE, "\\\\E.\\\\Q") + "\\E";
+    final static String UNDERSCORE;
+    final static String BACKSLASH;
+    final static String BACKSLASH_Q;
+    final static String BACKSLASH_E;
+    final static String DOT;
+
+    static {
+        BACKSLASH_E = "\\E";
+        BACKSLASH_Q = "\\Q";
+        UNDERSCORE = "_";
+        BACKSLASH = "\\";
+        DOT = ".";
+    }
+
+    public static int find(String word, String text) {
+        String regWord = BACKSLASH_Q + word.replaceAll(UNDERSCORE,
+                BACKSLASH + BACKSLASH_E + DOT + BACKSLASH + BACKSLASH_Q) + BACKSLASH_E;
         Matcher matcherWord = Pattern.compile(regWord).matcher(text);
         return matcherWord.find() ? matcherWord.start() : -1;
     }
